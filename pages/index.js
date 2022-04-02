7531a61834792d28b6c17ef202d2de09c12627aa
unknown <mahmudulh883@gmail.com>
2022-04-02 20:59:08 +0600

    updated

--- pages/index.js
+++ pages/index.js
@@ -25,13 +25,13 @@ export default function Home() {
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css"
           rel="stylesheet"
           integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We"
-          crossorigin="anonymous"
+          crossOrigin="anonymous"
         />
 
         <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.10.2/gsap.min.js"></script>
         <script
           src="https://kit.fontawesome.com/61a6132c09.js"
-          crossorigin="anonymous"
+          crossOrigin="anonymous"
         ></script>
         <link
           rel="stylesheet"
