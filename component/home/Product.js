682e8e1f6cda41e449adab4b549b3e3130a77fd6
unknown <mahmudulh883@gmail.com>
2022-05-15 16:19:02 +0600

    responsive card

--- component/home/Product.js
+++ component/home/Product.js
@@ -6,178 +6,309 @@ function Product() {
       <h3 className="fs-1 fw-bolder  " style={{ color: "#ff8095" }}>
         Top selected
       </h3>
-      <div className="   ">
-        <div className="row text-center my-4 py-4  ">
-          <div className="col-sm-12 col-md-4 p-2  ">
-            <div class="card border-0 " style={{ width: "18rem" }}>
-              <img
-                src="/images/jacket-1.png"
-                alt=""
-                className="card-img-top  p-2 "
-              />
+      <div className="row text-center my-4 py-4  ">
+        <div className="col-sm-12 col-md-4 p-2  ">
+          <div className="card border-0 " style={{ width: "18rem" }}>
+            <img
+              src="/images/jacket-1.png"
+              alt=""
+              className="card-img-top  p-2 "
+            />
 
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="text-center">550</p>
-                <button
-                  className="btn rounded-pill p-2 px-3"
-                  style={{
-                    backgroundColor: "#FFDDDE",
-                    color: "#ff8095",
-                    border: 0,
-                  }}
-                >
-                  BUY NOW
-                </button>
-              </div>
-            </div>
-          </div>{" "}
-          <div className="col-sm-12 col-md-4 p-2  ">
-            <div class="card border-0 " style={{ width: "18rem" }}>
-              <img
-                src="/images/jacket-1.png"
-                alt=""
-                className="card-img-top  p-2 "
-              />
-
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="text-center">550</p>
-                <button
-                  className="btn rounded-pill p-2 px-3"
-                  style={{
-                    backgroundColor: "#FFDDDE",
-                    color: "#ff8095",
-                    border: 0,
-                  }}
-                >
-                  BUY NOW
-                </button>
-              </div>
-            </div>
-          </div>{" "}
-          <div className="col-sm-12 col-md-4 p-2  ">
-            <div class="card border-0 " style={{ width: "18rem" }}>
-              <img
-                src="/images/jacket-1.png"
-                alt=""
-                className="card-img-top  p-2 "
-              />
-
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="text-center">550</p>
-                <button
-                  className="btn rounded-pill p-2 px-3"
-                  style={{
-                    backgroundColor: "#FFDDDE",
-                    color: "#ff8095",
-                    border: 0,
-                  }}
-                >
-                  BUY NOW
-                </button>
-              </div>
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="text-center">550</p>
+              <button
+                className="btn rounded-pill p-2 px-3"
+                style={{
+                  backgroundColor: "#FFDDDE",
+                  color: "#ff8095",
+                  border: 0,
+                }}
+              >
+                BUY NOW
+              </button>
             </div>
           </div>
-          <div className="col-sm-12 col-md-4 p-2  2">
-            <div class="card border-0 " style={{ width: "18rem" }}>
-              <img
-                src="/images/jacket-1.png"
-                alt=""
-                className="card-img-top  p-2 "
-              />
+        </div>{" "}
+        <div className="col-sm-12 col-md-4 p-2  ">
+          <div className="card border-0 " style={{ width: "18rem" }}>
+            <img
+              src="/images/jacket-1.png"
+              alt=""
+              className="card-img-top  p-2 "
+            />
 
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="text-center">550</p>
-                <button
-                  className="btn rounded-pill p-2 px-3"
-                  style={{
-                    backgroundColor: "#FFDDDE",
-                    color: "#ff8095",
-                    border: 0,
-                  }}
-                >
-                  BUY NOW
-                </button>
-              </div>
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="text-center">550</p>
+              <button
+                className="btn rounded-pill p-2 px-3"
+                style={{
+                  backgroundColor: "#FFDDDE",
+                  color: "#ff8095",
+                  border: 0,
+                }}
+              >
+                BUY NOW
+              </button>
             </div>
           </div>
-          <div className="col-sm-12 col-md-4 p-2  ">
-            <div class="card border-0 " style={{ width: "18rem" }}>
-              <img
-                src="/images/jacket-1.png"
-                alt=""
-                className="card-img-top  p-2 "
-              />
+        </div>{" "}
+        <div className="col-sm-12 col-md-4 p-2  ">
+          <div className="card border-0 " style={{ width: "18rem" }}>
+            <img
+              src="/images/jacket-1.png"
+              alt=""
+              className="card-img-top  p-2 "
+            />
 
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="text-center">550</p>
-                <button
-                  className="btn rounded-pill p-2 px-3"
-                  style={{
-                    backgroundColor: "#FFDDDE",
-                    color: "#ff8095",
-                    border: 0,
-                  }}
-                >
-                  BUY NOW
-                </button>
-              </div>
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="text-center">550</p>
+              <button
+                className="btn rounded-pill p-2 px-3"
+                style={{
+                  backgroundColor: "#FFDDDE",
+                  color: "#ff8095",
+                  border: 0,
+                }}
+              >
+                BUY NOW
+              </button>
             </div>
           </div>
-          <div className="col-sm-12 col-md-4 p-2">
-            <div class="card border-0 " style={{ width: "18rem" }}>
-              <img
-                src="/images/jacket-1.png"
-                alt=""
-                className="card-img-top  p-2 "
-              />
+        </div>
+        <div className="col-sm-12 col-md-4 p-2  2">
+          <div className="card border-0 " style={{ width: "18rem" }}>
+            <img
+              src="/images/jacket-1.png"
+              alt=""
+              className="card-img-top  p-2 "
+            />
 
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="text-center">550</p>
-                <button
-                  className="btn rounded-pill p-2 px-3"
-                  style={{
-                    backgroundColor: "#FFDDDE",
-                    color: "#ff8095",
-                    border: 0,
-                  }}
-                >
-                  BUY NOW
-                </button>
-              </div>
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="text-center">550</p>
+              <button
+                className="btn rounded-pill p-2 px-3"
+                style={{
+                  backgroundColor: "#FFDDDE",
+                  color: "#ff8095",
+                  border: 0,
+                }}
+              >
+                BUY NOW
+              </button>
             </div>
           </div>
-          <div className="col-sm-12 col-md-4 p-2">
-            <div class="card border-0 " style={{ width: "18rem" }}>
-              <img
-                src="/images/jacket-1.png"
-                alt=""
-                className="card-img-top  p-2 "
-              />
+        </div>
+        <div className="col-sm-12 col-md-4 p-2  ">
+          <div className="card border-0 " style={{ width: "18rem" }}>
+            <img
+              src="/images/jacket-1.png"
+              alt=""
+              className="card-img-top  p-2 "
+            />
 
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="text-center">550</p>
-                <button
-                  className="btn rounded-pill p-2 px-3"
-                  style={{
-                    backgroundColor: "#FFDDDE",
-                    color: "#ff8095",
-                    border: 0,
-                  }}
-                >
-                  BUY NOW
-                </button>
-              </div>
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="text-center">550</p>
+              <button
+                className="btn rounded-pill p-2 px-3"
+                style={{
+                  backgroundColor: "#FFDDDE",
+                  color: "#ff8095",
+                  border: 0,
+                }}
+              >
+                BUY NOW
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="col-sm-12 col-md-4 p-2">
+          <div className="card border-0 " style={{ width: "18rem" }}>
+            <img
+              src="/images/jacket-1.png"
+              alt=""
+              className="card-img-top  p-2 "
+            />
+
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="text-center">550</p>
+              <button
+                className="btn rounded-pill p-2 px-3"
+                style={{
+                  backgroundColor: "#FFDDDE",
+                  color: "#ff8095",
+                  border: 0,
+                }}
+              >
+                BUY NOW
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="col-sm-12 col-md-4 p-2">
+          <div className="card border-0 " style={{ width: "18rem" }}>
+            <img
+              src="/images/jacket-1.png"
+              alt=""
+              className="card-img-top  p-2 "
+            />
+
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="text-center">550</p>
+              <button
+                className="btn rounded-pill p-2 px-3"
+                style={{
+                  backgroundColor: "#FFDDDE",
+                  color: "#ff8095",
+                  border: 0,
+                }}
+              >
+                BUY NOW
+              </button>
             </div>
           </div>
         </div>
       </div>
+
+      {/* test */}
+
+      <section id="shop" className="container">
+        <div className="text-center row row-cols-1 gy-md-2 justify-content-center">
+          <h2 className="px-lg-5  fw-bolder">Fetured Bikes 2021</h2>
+          <p className="text-color">Best bike collection.</p>
+          <hr className="bg-warning w-25" />
+        </div>
+
+        <div className="  row justify-content-center  g-4 my-lg-5 px-md-5 ">
+          <div className="col-12 col-lg-4  rounded-3 justify-item-center">
+            <img
+              className="d-block m-auto p-2 convert-grey-image"
+              width="288"
+              height="343"
+              src="images/jacket-1.png"
+              alt=""
+            />
+          </div>
+          <div className="col-12 col-lg-4 rounded-3  ">
+            <img
+              className="d-block m-auto p-2 "
+              width="288"
+              height="343"
+              src="images/jacket-1.png"
+              alt=""
+            />
+          </div>
+          <div className="col-12  col-lg-4  rounded-3 convert-grey-image">
+            <img
+              className="d-block m-auto p-2"
+              width="288"
+              height="343"
+              src="images/jacket-1.png"
+              alt=""
+            />
+          </div>
+        </div>
+      </section>
+      {/* test */}
+      {/* <!-- ******Happy Clients says section*****  -->        */}
+      {/* <section className="container my-lg-3 py-4">
+        <div className="text-center d-flex justify-content-center p-lg-5 m-1">
+          <h3>Happy</h3>
+          <h3 className="text-color ms-2"> Clients says</h3>
+        </div>
+      </section> */}
+      {/* <!--1st card added --> */}
+      <section className="container">
+        <div className=" row g-4 justify-content-around">
+          <div className="  col-12 col-md-3 col-lg-3 card text-center p-2">
+            <div className="card-header border-bottom-0 bg-white">
+              <img
+                width="149px"
+                height="149px"
+                className="p-3"
+                src="images/a1.png"
+                alt=""
+              />
+            </div>
+            <div className="card-body my-2">
+              <p className="card-title my-2">
+                Slate helps you see how many more days you need to work to reach
+                your financial goal for the month and year.
+              </p>
+              <div className="my-3">
+                <i className="fas fa-star filled" aria-hidden="true"></i>
+                <i className="fas fa-star filled" aria-hidden="true"></i>
+                <i className="fas fa-star filled" aria-hidden="true"></i>
+                <i className="fas fa-star filled" aria-hidden="true"></i>
+                <i className="fas fa-star empty" aria-hidden="true"></i>
+              </div>
+              <h4 className="text-primary my-3">Regina Miles</h4>
+              <h6>Banker</h6>
+            </div>
+          </div>
+          {/* <!-- 2nd card added --> */}
+          <div className="col-12 col-md-3 col-lg-3 card text-center p-2 ">
+            <div className="card-header border-bottom-0 bg-white">
+              <img
+                width="149px"
+                height="149px"
+                className="p-3"
+                src="images/a2.png"
+                alt=""
+              />
+            </div>
+            <div className="card-body my-2">
+              <p className="card-title my-2">
+                Slate helps you see how many more days you need to work to reach
+                your financial goal for the month and year.
+              </p>
+              <div className="my-3">
+                <i className="fas fa-star filled" aria-hidden="true"></i>
+                <i className="fas fa-star filled" aria-hidden="true"></i>
+                <i className="fas fa-star filled" aria-hidden="true"></i>
+                <i className="fas fa-star filled" aria-hidden="true"></i>
+                <i className="fas fa-star empty" aria-hidden="true"></i>
+              </div>
+              <h4 className="text-primary my-3">Regina Miles</h4>
+              <h6>Banker</h6>
+            </div>
+          </div>
+          {/* <!-- 3rd card --> */}
+          <div className="col-12 col-md-3 col-lg-3 card text-center  p-2">
+            <div className="card-header border-bottom-0 bg-white">
+              <img
+                width="149px"
+                height="149px"
+                className="p-3 rounded-circle"
+                src="images/a3.png"
+                alt=""
+              />
+            </div>
+            <div className="card-body my-2">
+              <p className="card-title my-2">
+                Slate helps you see how many more days you need to work to reach
+                your financial goal for the month and year.
+              </p>
+              <div className="my-3">
+                <i className="fas fa-star filled" aria-hidden="true"></i>
+                <i className="fas fa-star filled" aria-hidden="true"></i>
+                <i className="fas fa-star filled" aria-hidden="true"></i>
+                <i className="fas fa-star filled" aria-hidden="true"></i>
+                <i className="fas fa-star empty" aria-hidden="true"></i>
+              </div>
+              <h4 className="text-primary my-3">Regina Miles</h4>
+              <h6>Banker</h6>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
